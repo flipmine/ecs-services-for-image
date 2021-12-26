262b18e28f28643406de20ab1c6e6661233fd459
David Gillis <david@flipmine.com>
2021-12-26 14:49:47 -0500

    move code over from monorepo

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const {
+  ECS,
+} = require('@aws-sdk/client-ecs')
+const ecsClient = new ECS({ region: 'us-west-2' })
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+      const imageFamily = core.getInput('imageFamily', { required: true })
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+      const listClusterResponse = await ecsClient.listClusters({ maxResults: 100 })
+      const clusterArns = listClusterResponse.clusterArns
+      const describeClustersResponse = await ecsClient.describeClusters({
+        clusters: clusterArns,
+      })
+      const clusters = describeClustersResponse.clusters
 
-    core.setOutput('time', new Date().toTimeString());
+      const describeServicesResponses = await Promise.all(
+        clusters.map(async cluster => {
+          const listServicesResponse = await ecsClient.listServices({
+            cluster: cluster.clusterArn,
+          })
+          const serviceArns = listServicesResponse.serviceArns
+          if (serviceArns.length) {
+            return ecsClient.describeServices({
+              cluster: cluster.clusterArn,
+              services: serviceArns,
+            })
+          } else {
+            return null
+          }
+        }),
+      )
+
+      const services = describeServicesResponses
+        .filter(response => response !== null)
+        .flatMap(response => response.services)
+
+      const allSimplifiedServices = await Promise.all(
+        services.map(async service => {
+          const describeTaskDefinitionResponse = await ecsClient.describeTaskDefinition(
+            {
+              taskDefinition: service.taskDefinition,
+            },
+          )
+          const taskDefinition = describeTaskDefinitionResponse.taskDefinition
+          const clusterName = clusters.find(
+            cluster => cluster.clusterArn === service.clusterArn,
+          ).clusterName
+
+          return {
+            serviceName: service.serviceName,
+            taskDefinitionFamily: taskDefinition.family,
+            clusterName,
+            images: taskDefinition.containerDefinitions.map(
+              containerDef => containerDef.image,
+            ),
+            deploymentController:
+              service.deploymentController && service.deploymentController.type,
+          }
+        }),
+      )
+
+      const regexToMatch = new RegExp(`/${imageFamily}:`)
+      const filteredServices = allSimplifiedServices
+        .filter(service => service.images.some(image => image.match(regexToMatch)))
+      core.setOutput('services', filteredServices)
   } catch (error) {
     core.setFailed(error.message);
   }
